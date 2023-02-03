a34ef6c91fb259f6b284d5cfc70cdd3e8cb5153c
davyalex <alexkouamelan96pv@gmail.com>
2023-02-03 23:02:35 +0000

    optimize

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -12,10 +12,13 @@
   <meta property="og:url" content="@yield('url')">
 
   <!-- Favicons -->
-  <link href="assets_site/img/favicon.png" rel="icon">
+  <link href="assets_site/img/favicon-32x32.png" rel="icon">
   <link href="assets_site/img/apple-touch-icon.png" rel="apple-touch-icon">
   {{-- <link rel="preload" href="{{ asset('assets_site/img/person-1.jpg') }}" as="image"> --}}
-
+<link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets_site/')}}">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
   <!-- Google Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
