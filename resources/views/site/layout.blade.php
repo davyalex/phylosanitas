3b17515c0dec22bc7e26f702d4332a74912b32b3
davyalex <alexkouamelan96pv@gmail.com>
2024-01-20 22:58:02 +0000

    optimize

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -33,6 +33,7 @@
         href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500&family=Inter:wght@400;500&family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap"
         rel="stylesheet">
 
+
     <!-- Vendor CSS Files -->
     <link href="{{ asset('assets_site/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets_site/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
@@ -266,7 +267,7 @@
             class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src="{{ asset('assets_site/vendor/bootstrap/js/bootstrap.bundle.min.js') }} "defer></script>
+    <script src="{{ asset('assets_site/vendor/bootstrap/js/bootstrap.bundle.min.js') }} " defer></script>
     <script src="{{ asset('assets_site/vendor/swiper/swiper-bundle.min.js') }}" defer></script>
     <script src="{{ asset('assets_site/vendor/glightbox/js/glightbox.min.js') }}" defer></script>
     <script src="{{ asset('assets_site/vendor/aos/aos.js') }}" defer></script>
