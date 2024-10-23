b102c652548108272e7b15ac3066b011fc78d290
davyalex <alexkouamelan96pv@gmail.com>
2024-10-23 22:02:32 +0000

    optimiz

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -48,8 +48,9 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
   
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6925205610540207"
+     crossorigin="anonymous"></script>
 
-  
 </head>
 <body>
 
